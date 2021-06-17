efad4dffa84caabd33c25e383419a25c4b755b32
신우진 <w34538y@gmail.com>
2021-06-17 16:45:07 +0900

    edit detailController

--- mvc_workspace/controlloers/detailController.js
+++ mvc_workspace/controlloers/detailController.js
@@ -1,6 +1,26 @@
 const DetailController  = function(){
     const detailViewRender = function(req,res){
-        console.log("주소를 통해 넘겨받은 글 번호 :", req.params.index);
+        
+        const getData = function(){
+            return new Promise(function(resolve){
+                console.log("주소를 통해 넘겨받은 글 번호 :", req.params.index);
+                // 배열 데이터에서 req 객체를 통해 받은 글 번호에 해당하는 것만 꺼내온다. 
+                const array = test_model.testReturnArray();
+
+                const data = array.find(function(v){
+                    if(v.index === parseInt(req.params.index)){
+                        return v;
+                    }
+                });
+                resolve(data);
+            });
+        }
+    }
+
+    const renderView = function(data){
+        return new Promise(function(){
+            res.render('detail', data);
+        })
     }
     return {
         detailView : function(req,res){
